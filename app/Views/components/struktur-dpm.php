b852473c4e9b53787155e3db84513cc8f9d911f7
jd <msirojuddin26@gmail.com>
2023-06-30 10:57:02 +0700

    nambah component

---
+++ app/Views/components/struktur-dpm.php
@@ -0,0 +1,64 @@
+<!-- Struktur DPM -->
+
+<section>
+  <figure class="org-chart cf">
+    <ul class="administration struktur">
+      <li>
+        <ul class="director struktur">
+          <li class="struktur ketua"><a class="struktur" href="#" data-toggle="modal" data-target="#modalKetua"><span class="struktur">Ketua
+                DPM</span></a></li>
+          <li class="struktur"><a class="struktur" href="#" data-toggle="modal" data-target="#modalWakilKetua"><span class="struktur">Wakil Ketua
+                DPM</span></a></li>
+        </ul>
+        <ul class="subdirector struktur">
+          <li class="struktur"><a class="struktur" href="#" data-toggle="modal" data-target="#modalSekretaris"><span class="struktur">Sekretaris</span></a></li>
+        </ul>
+        <ul class="departments cf struktur">
+
+          <li class="struktur"><a class="struktur" href="#" data-toggle="modal" data-target="#modalBendahara"><span class="struktur">Bendahara</span></a></li>
+
+          <li class="department dep-a struktur">
+                                  <a class="struktur" href="#" data-toggle="modal" data-target="#modalKomisiSatu">
+                                    <span class="struktur"><i class="fas fa-fw fa-gavel"></i><br>Komisi
+                                      I</span>
+                                  </a>
+                                </li>
+                                <li class="department dep-b struktur">
+
+                                  <a class="struktur" href="#" data-toggle="modal" data-target="#modalHpd">
+                                    <span class="struktur"><i class="fas fa-fw fa-camera"></i><br>Pubmedkraf</span>
+                                  </a>
+
+                                </li>
+                                <li class="department dep-c struktur">
+
+                                  <a class="struktur" href="#" data-toggle="modal" data-target="#modalKomisiDua">
+                                    <span class="struktur"><i class="fas fa-fw fa-balance-scale"></i><br>Komisi
+                                      II</span>
+                                  </a>
+                                </li>
+                                <li class="department dep-d struktur">
+
+                                  <a class="struktur" href="#" data-toggle="modal" data-target="#modalKomisiTiga">
+                                    <span class="struktur"><i class="fas fa-fw fa-eye"></i><br>Komisi
+                                      III</span></a>
+                                </li>
+                                <li class="department dep-e struktur">
+                                  <a class="struktur" href="#" data-toggle="modal" data-target="#modalIt">
+                                    <span class="struktur"><i class="fas fa-fw fa-code"></i><br>IT</span>
+                                  </a>
+                                </li>
+                                <li class="department dep-f struktur">
+                                  <a class="struktur" href="#" data-toggle="modal" data-target="#modalKomisiEmpat">
+                                    <span class="struktur"><i class="fas fa-fw fa-comments"></i><br>Komisi IV</span>
+                                  </a>
+                                </li>
+        </ul>
+      </li>
+    </ul>
+    </li>
+    </ul>
+  </figure>
+</section>
+
+<!-- End of Struktur DPM -->
