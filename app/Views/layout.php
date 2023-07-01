b7dbb8b1628b4404bcd7599741ce93b86c68f543
arfiandys <222011791@stis.ac.id>
2023-07-01 21:43:38 +0700

    final galeri

--- app/Views/layout.php
+++ app/Views/layout.php
@@ -150,6 +150,54 @@
             e.preventDefault();
         });
     </script>
+
+    <script>
+        filterSelection("galeri_semua")
+
+        function filterSelection(c) {
+            var x, i;
+            x = document.getElementsByClassName("filterDiv");
+            if (c == "galeri_semua") c = "";
+            for (i = 0; i < x.length; i++) {
+                w3RemoveClass(x[i], "show-galeri");
+                if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show-galeri");
+            }
+        }
+
+        function w3AddClass(element, name) {
+            var i, arr1, arr2;
+            arr1 = element.className.split(" ");
+            arr2 = name.split(" ");
+            for (i = 0; i < arr2.length; i++) {
+                if (arr1.indexOf(arr2[i]) == -1) {
+                    element.className += " " + arr2[i];
+                }
+            }
+        }
+
+        function w3RemoveClass(element, name) {
+            var i, arr1, arr2;
+            arr1 = element.className.split(" ");
+            arr2 = name.split(" ");
+            for (i = 0; i < arr2.length; i++) {
+                while (arr1.indexOf(arr2[i]) > -1) {
+                    arr1.splice(arr1.indexOf(arr2[i]), 1);
+                }
+            }
+            element.className = arr1.join(" ");
+        }
+
+        // Add active class to the current button (highlight it)
+        var btnContainer = document.getElementById("myBtnContainer");
+        var btns = btnContainer.getElementsByClassName("btn-galeri");
+        for (var i = 0; i < btns.length; i++) {
+            btns[i].addEventListener("click", function() {
+                var current = document.getElementsByClassName("activ");
+                current[0].className = current[0].className.replace(" activ", "");
+                this.className += " activ";
+            });
+        }
+    </script>
 </body>
 
 </html>
