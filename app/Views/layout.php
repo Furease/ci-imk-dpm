41e83e3cc9ea6f41b2a2e936653f9b68d52a406b
arfiandys <222011791@stis.ac.id>
2023-07-02 11:57:25 +0700

    final aspirasi

--- app/Views/layout.php
+++ app/Views/layout.php
@@ -72,16 +72,16 @@
     <div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
         <div class="modal-dialog" role="document">
             <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLabel">Yakin Untuk Logout?</h5>
+                <div class="modal-header border-0">
+                    <h4 class="modal-title text-black" id="exampleModalLabel">Yakin Untuk Logout?</h4>
                     <button class="close" type="button" data-dismiss="modal" aria-label="Close">
                         <span aria-hidden="true">×</span>
                     </button>
                 </div>
-                <div class="modal-body">Apabila anda yakin untuk logout anda dapat menekan tombol iya, jika anda ingin kembali ke halaman web anda bisa menekan tombol tidak.
+                <div class="modal-body text-justify">Apabila anda yakin untuk logout anda dapat menekan tombol iya, jika anda ingin kembali ke halaman web anda bisa menekan tombol tidak.
                 </div>
-                <div class="modal-footer">
-                    <a class="btn btn-danger" href="/">Iya</a>
+                <div class="modal-footer border-0 justify-content-start">
+                    <a class="btn btn-cancle" href="/">Iya</a>
                     <button class="btn btn-primary" type="button" data-dismiss="modal">Tidak</button>
                 </div>
             </div>
@@ -92,17 +92,17 @@
     <div class="modal fade" id="aspirasiModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
         <div class="modal-dialog" role="document">
             <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLabel">Apakah anda sudah yakin untuk mengirim aspirasi?</h5>
+                <div class="modal-header border-0">
+                    <h4 class="modal-title text-black" id="exampleModalLabel">Anda Yakin Untuk Mengirim Aspirasi?</h4>
                     <button class="close" type="button" data-dismiss="modal" aria-label="Close">
                         <span aria-hidden="true">×</span>
                     </button>
                 </div>
-                <div class="modal-body">Apabila anda yakin untuk mengirim aspirasi anda dapat menekan tombol iya, jika anda ingin kembali ke halaman web anda bisa menekan tombol tidak.
+                <div class="modal-body text-justify">Apabila anda yakin untuk mengirim aspirasi anda dapat menekan tombol iya, jika anda ingin kembali ke halaman web anda bisa menekan tombol tidak.
                 </div>
-                <div class="modal-footer">
-                    <a class="btn btn-danger" href="/aspirasi">Iya</a>
-                    <button class="btn btn-primary" type="button" data-dismiss="modal">Tidak</button>
+                <div class="modal-footer border-0 justify-content-start">
+                    <a class="btn btn-primary" href="/aspirasi">Iya</a>
+                    <button class="btn btn-cancle" type="button" data-dismiss="modal">Tidak</button>
                 </div>
             </div>
         </div>
